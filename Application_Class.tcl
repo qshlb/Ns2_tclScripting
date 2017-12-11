3619251d50ef05a9707bd9e2819331b1d4c9bbaf
YoloHealth <jainswati048@gmail.com>
2017-12-11 14:39:29 +0530

    Application Class Basics

---
+++ Application_Class.tcl
@@ -0,0 +1,18 @@
+# Two types :- Traffic generator & Simulated Applicaton
+
+#---Traffic Gen :- {4 types} Exponential Traffic Generator, Pareto Traffic Generator, CBR, Trace
+
+# use as
+
+set exp [Application/Traffic/Exponential]
+set par [Application/Traffic/Pareto]
+set cbr [Application/Traffic/CBR]
+set tr [Application/Traffic/Trace]
+
+
+#---Simulated application {2 type} :- FTP, Telnet
+
+set ftp [Application/FTP]
+set tel [Application/Telnet]
+
+
